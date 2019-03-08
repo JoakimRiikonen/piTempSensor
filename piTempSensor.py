a7f7ed2b0f8bac7543386e8b4d2db56fac3d2b6b
unknown <joakim.riiikonen@gmail.com>
2019-03-08 15:16:59 +0200

    fixed floating point decimals

--- piTempSensor.py
+++ piTempSensor.py
@@ -50,7 +50,8 @@ class GUI:
         self.root.destroy()
 
     def update_text(self):
-        self.tempLabel['text'] = str(readSensor(self.sensor)) + "C"
+        # self.tempLabel['text'] = str(readSensor(self.sensor)) + "C"
+        self.tempLabel['text'] = "%0.1f C" % readSensor(self.sensor)
         threading.Timer(1000, self.update_text).start()
 
 
